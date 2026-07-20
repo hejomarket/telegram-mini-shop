9e64cebe734e6c758673c46724ce478bb248cae4
hejomarket <hejomarket@gmail.com>
2026-07-20 19:27:32 +0700

    Fix Next.js TypeScript foundation

---
+++ types/node-env.d.ts
@@ -0,0 +1,6 @@
+declare const process: {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL?: string;
+    SUPABASE_SERVICE_ROLE_KEY?: string;
+  };
+};
---
+++ types/react-global.d.ts
@@ -0,0 +1,4 @@
+declare namespace React {
+  type ReactNode = import('react').ReactNode;
+  type FormEvent = import('react').FormEvent;
+}
